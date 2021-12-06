7dc38ed571b064f23cafc78fd5e3e12e00e7e1da
Nedzhat <nexuak@gmail.com>
2021-12-06 19:00:27 +0200

    add last task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+const data = {};
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  data.email = email.value;
+  data.password = password.value;
+  console.log(data);
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const spanRef = document.querySelector('.color');
+const btnRef = document.querySelector('.change-color');
+const bodyRef = document.querySelector('body');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function changeClr() {
+  const numColor = getRandomHexColor();
+  spanRef.textContent = numColor;
+  bodyRef.style.background = numColor;
+}
+
+btnRef.addEventListener('click', changeClr);
